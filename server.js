cc14d0f3cd1faa2434bc54f69e9600ce1820ad2d
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-11 13:53:49 +0545

    declare routes and connect mlab database

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
 
-app.get('/',(req,res)=> {
-    console.log('helloworld');
-})
+// connect MongoDB DataBase
+connectDB();
 
+// define routes
+app.use('/api/users',require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+// run server
 app.listen(PORT,()=> {
     console.log(`Server started on port ${PORT}`)
 });
