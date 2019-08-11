345b31938ed07a0bd6301407b6f2375495b7d4b9
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-11 17:08:57 +0545

    express-validator ,registering user and response jwt token

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db,{useNewUrlParser:true});
+        await mongoose.connect(db,{useNewUrlParser:true,
+        useCreateIndex:true});
         console.log('Mongodb connected');
     } catch (error) {
         console.log(error.message);
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const connectDB = require('./config/db')
 // connect MongoDB DataBase
 connectDB();
 
+// bodyparser middleware
+app.use(express.json({extended:false}))
+
 // define routes
 app.use('/api/users',require('./routes/api/users'));
 // app.use('/api/auth', require('./routes/api/auth'));
