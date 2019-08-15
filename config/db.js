4ac691e8015bece1379b306ea626491850bb3578
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-15 13:05:16 +0545

    create and update profile

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI')
 const connectDB = async () => {
     try {
         await mongoose.connect(db,{useNewUrlParser:true,
-        useCreateIndex:true});
+            useCreateIndex: true, useFindAndModify: false});
         console.log('Mongodb connected');
     } catch (error) {
         console.log(error.message);
