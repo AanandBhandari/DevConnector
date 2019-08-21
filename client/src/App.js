bfab0f82a76e261181f511eeab06f46c78905fde
AanandBhandari <aanandbhandari143@gmail.com>
2019-08-22 00:09:06 +0545

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import NavBar from './components/layout/NavBar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 function App() {
   return (
+    <Router>
     <Fragment>
-      <h1>helloworld</h1>
       <NavBar/>
-      <Landing/>
+      <Route exact path='/' component={Landing}/>
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+    </Router>
   );
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
     return (
         <section class="landing">
             <div class="dark-overlay">
                 <div class="landing-inner">
-                    <h1 class="x-large">Developer Connector</h1>
+                    <h1 class="x-large">Dev Call</h1>
                     <p class="lead">
                         Create a developer profile/portfolio, share posts and get help from
                         other developers
           </p>
                     <div class="buttons">
-                        <a href="register.html" class="btn btn-primary">Sign Up</a>
-                        <a href="login.html" class="btn btn-light">Login</a>
+                        <Link to="/register" class="btn btn-primary">Sign Up</Link>
+                        <Link to="/Slogin" class="btn btn-light">Login</Link>
                     </div>
                 </div>
             </div>
