f1a7f516063d9275b9fb82f75614c59afbefeb2a
AanandBhandari <aanandbhandari143@gmail.com>
2019-09-01 14:09:56 +0545

    make profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -17,3 +17,32 @@ export const getCurrentProfile = () => async dispatch => {
         });
     }
 };
+// Create or Update profile
+export const createProfile = (formData, history, edit =false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile',formData,config)
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+        dispatch(setAlert(edit? 'Profile Updated': 'Profile Created','success'));
+        if(!edit) {
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if (errors) {
+            // setAlert has already dispatch method why do we need to call here also
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err, status: err.response.status }
+        });
+    }
+}
