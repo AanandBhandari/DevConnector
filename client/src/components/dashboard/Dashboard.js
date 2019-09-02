22baf51eecba7b213196d147da94ebfa08b02e34
AanandBhandari <aanandbhandari143@gmail.com>
2019-09-02 15:07:15 +0545

    delete education, experience and account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,13 @@ import React,{Fragment,useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {getCurrentProfile} from '../../actions/profile'
+import {getCurrentProfile, deleteAccount} from '../../actions/profile'
 import Spiner from '../layout/Spiner'
 import DashboardActions from './DashboardActions'
 import Experience from './Experience'
 import Education from './Education'
 
-const Dashboard = ({ getCurrentProfile, auth:{user }, profile:{profile, loading} }) => {
+const Dashboard = ({ getCurrentProfile, auth:{user }, profile:{profile, loading}, deleteAccount }) => {
     useEffect(()=> {
         getCurrentProfile();
     },[getCurrentProfile])
@@ -20,6 +20,11 @@ const Dashboard = ({ getCurrentProfile, auth:{user }, profile:{profile, loading}
         {profile !== null ? <Fragment><DashboardActions/>
         <Experience experience={profile.experience}/>
             <Education education={profile.education} />
+        <div className='my-2'>
+            <button onClick={()=>deleteAccount()} className ='btn btn-danger'>
+                <i className='fas fa-user-minus'></i> Delete My Account
+            </button>
+        </div>
         </Fragment>:<Fragment><p>
             You have not yet setup a profile, please add some info 
         </p>
@@ -31,7 +36,8 @@ const Dashboard = ({ getCurrentProfile, auth:{user }, profile:{profile, loading}
 Dashboard.propTypes = {
     getCurrentProfile : PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired
+    profile: PropTypes.object.isRequired,
+    deleteAccount: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -39,4 +45,5 @@ const mapStateToProps = state => ({
     profile: state.profile
 })
 
-export default connect(mapStateToProps,{getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps,{getCurrentProfile ,deleteAccount})(Dashboard);
+ 
--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -48,7 +48,7 @@ const AddEducation = ({ addEducation, history }) => {
                         checked={current} value={current} onChange={e => {
                             setFormData({ ...formData, current: !current })
                             toggleDisabled(!toDateDisable)
-                        }} /> {' '}Current Job</p>
+                        }} /> {' '}Current School</p>
                 </div>
                 <div className="form-group">
                     <h4>To Date</h4>
